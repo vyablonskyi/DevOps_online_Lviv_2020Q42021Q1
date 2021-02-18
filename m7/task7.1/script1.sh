3c02f321b0ba7edd084f49fa2ed219300ad37115
vyablonskyi <75807913+vyablonskyi@users.noreply.github.com>
2021-02-19 00:18:00 +0200

    Create script1.sh

---
+++ m7/task7.1/script1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function nmapcheck(){
+  nmap -v > /dev/null 2>&1
+  if [[ "$?" -gt 0 ]];
+    then
+      if [ -e /etc/debian_version ]
+        then
+          sudo apt-get -y install nmap> /dev/null 2>&1
+        elif [ -e /etc/redhat-release ]
+          then
+            yum -y install nmap > /dev/null 2>&1
+      fi
+  fi
+}
+
+function wrong(){
+  printf "\nPlease run the script with one of the following parameters:\n"
+  printf "\n--all <subnet>    - displays the IP addresses and symbolic names of all hosts in the specific subnet where subnet should be in the IP_address/mask format"
+  printf "\n--target <host>   - displays a list of open system TCP ports of specific host where <host> may be IP address or FQDN \n"
+}
+
+function showhosts(){
+  printf "\nList of all accessible machines in the $1 subnet\n"
+  nmap -sP $1 | grep report | awk '{print $6,$5}'
+}
+
+function showports(){
+  printf "\nList of open TCP ports on the $1 machine:\n"
+  nmap -sT $1 | grep open | awk -F / '{print $1}'
+}
+
+if  [ "$#" == "0" ]
+  then
+    wrong
+  else
+    nmapcheck
+    case $1 in
+       --all) showhosts $2
+       ;;
+       --target) showports $2
+       ;;
+       *) wrong
+       ;;
+     esac
+fi
